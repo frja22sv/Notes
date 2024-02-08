79cb9554ce16467dc85ed6845029a849ed5a9305
frja22sv <129272707+frja22sv@users.noreply.github.com>
2024-02-08 16:23:08 +0100

    1.0

--- app/src/main/java/com/example/notes/EditScreen.kt
+++ app/src/main/java/com/example/notes/EditScreen.kt
@@ -1,9 +1,9 @@
 package com.example.notes
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.TextField
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,8 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.input.TextFieldValue // Added TextFieldValue import
-import androidx.compose.ui.text.input.TextFieldValue.Companion // Added TextFieldValue.Companion import
 import androidx.navigation.NavController
 
 @Composable
@@ -30,38 +28,45 @@ fun EditScreen( navController: NavController,list: MutableList<Element>, modifie
     ){
         var title by remember { mutableStateOf(currentElement.title) }
         var text by remember { mutableStateOf(currentElement.text) }
+        var errorMessage = remember{ mutableStateOf("")}
 
-        var minTitleChar = 3
-        var maxTitleChar = 50
-        var maxTextChar = 150
+        val minTitleChar = 3
+        val maxTitleChar = 50
+        val maxTextChar = 120
 
 
         Text("Edit:")
         TextField(
             value = title,
-            onValueChange = { if(it.length <= maxTitleChar){ //youtube video
-                title = it
-            } },
+            onValueChange = { title = it },
             label = { Text("Title") }
         )
         TextField(
             value = text,
-            onValueChange = { if(it.length <= maxTextChar){ //youtube video
-                text = it
-            }
-                                                          },
+            onValueChange = { text = it},
             label = { Text("Text") }
         )
         Button(onClick = {
-            if(title.length >=minTitleChar){
+            if(title.length >=minTitleChar && title.length < maxTitleChar && text.length < maxTextChar){
                 currentElement.title = title
                 currentElement.text = text
                 navController.navigate("List")
+            }
+            else if (text.length > maxTextChar){
+                errorMessage.value = "Error, the text length is to long."
+            }
+            else if (title.length > maxTitleChar){
+                errorMessage.value = "Error, the title length is to long."
+            }
+            else {
+                errorMessage.value = "Error, the title length is to short."
             }
 
         }) {
             Text("RETURN")
         }
-
+    Row {
+        Text(text = errorMessage.value)
+    }
     }
 }
